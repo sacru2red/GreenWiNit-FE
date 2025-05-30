cedb11cfae46625ac2fbeb445bdc8c2f3db18250
Jiwoong Song <jisongimsorry@gmail.com>
2025-05-30 18:03:35 +0900

    관리자페이지 기본 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.tsx";
+import Admin from "./pages/Admin.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/admin" element={<Admin />}></Route>
+    </Routes>
+  </BrowserRouter>
+);
---
+++ src/pages/Admin.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/components/ui/admin/Login";
+
+const Admin = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Admin;
