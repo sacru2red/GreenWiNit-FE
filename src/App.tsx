9475f5894ae98068f60d9363d7c1e93df25641a8
sacru2red <sacru2red@gmail.com>
2025-06-20 00:02:27 +0900

    change default screen to main

--- src/App.tsx
+++ src/App.tsx
@@ -30,9 +30,9 @@ function App() {
         }`}
       >
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/main" element={<Main />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/my" element={<MyPage />} />
+          <Route path="*" element={<Main />} />
         </Routes>
       </div>
     </>
