52286fc5f46b9e9dc6744f6f0b6e1d0409571c14
sacru2red <sacru2red@gmail.com>
2025-06-20 19:53:54 +0900

    remove font-jalnan

--- src/components/common/AppTitle.tsx
+++ src/components/common/AppTitle.tsx
@@ -2,7 +2,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 // 시안상에서는 font-size: 40px; 4xl -> 36px;
-const appTitleVariants = cva('font-jalnan !text-4xl text-mountain_meadow', {
+const appTitleVariants = cva('!text-4xl text-mountain_meadow', {
   variants: {},
   defaultVariants: {},
 })
