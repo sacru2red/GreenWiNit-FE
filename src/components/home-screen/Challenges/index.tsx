5263ed2c3ac26e5122ff7be890b49dcef14b25d5
sacru2red <sacru2red@gmail.com>
2025-07-01 10:16:47 +0900

    툴팁설정, primary 색상변경

--- src/components/home-screen/Challenges/index.tsx
+++ src/components/home-screen/Challenges/index.tsx
@@ -4,6 +4,9 @@ import { TabProps } from '@/components/home-screen/Challenges/type'
 import { useChallenges } from '@/hooks/useChallenges'
 import Challenge from '@/components/common/Challenge'
 import { useNavigate } from 'react-router-dom'
+import InfoOutlineIcon from '@mui/icons-material/InfoOutline'
+import { MOUNTAIN_MEADOW } from '@/constant/styles'
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
 
 const Challenges = () => {
   const [tab, setTab] = useState<TabProps['tab']>('individual')
@@ -15,9 +18,19 @@ const Challenges = () => {
     <div className="flex h-full flex-col">
       <Tab tab={tab} setTab={setTab} />
       <div className="flex h-0 w-full flex-[1_1_auto] flex-col p-4 pt-8">
-        <div className="flex flex-row gap-1">
+        <div className="flex flex-row items-center gap-1">
           <span className="text-xl font-bold text-[#404040]">{`${tab === 'individual' ? '개인' : '팀'} 챌린지`}</span>
-          <img src="/icons/infocircle.svg" />
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <InfoOutlineIcon htmlColor={MOUNTAIN_MEADOW} />
+            </TooltipTrigger>
+            <TooltipContent className="p-4 shadow-xl">
+              <p className="text-center">
+                원하는 챌린지에 참여 후<br />
+                [참여 챌린지]에서 인증해주세요!
+              </p>
+            </TooltipContent>
+          </Tooltip>
         </div>
         {/* @TODO: 실수로 y축 스크롤 적용함 다시 x축으로 변경작업 필요 */}
         <div className="mt-4 flex flex-[1_1_auto] flex-row flex-wrap gap-4 overflow-y-auto">
