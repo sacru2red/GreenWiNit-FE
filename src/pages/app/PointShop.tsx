0545229b3a30539b3164aed5bf7ad5f4afe8333b
beomjo <dongjun1315@naver.com>
2025-07-12 12:44:05 +0900

    feat(#23): 유저 포인트 현황 컴포넌트 생성

--- src/pages/app/PointShop.tsx
+++ src/pages/app/PointShop.tsx
@@ -1,9 +1,19 @@
 import BottomNavigation from '@/components/common/BottomNav'
 import PageContainer from '@/components/common/PageContainer'
+import UserStatusbar from '@/components/shop-screen/UserStatusbar'
+import { useUserStatus } from '@/hooks/useUserStatus'
 
 function PointShop() {
+  const { data: userStatus } = useUserStatus()
+
   return (
     <PageContainer>
+      <div className="flex h-[48px] w-full items-center justify-center bg-white py-8">
+        <span className="text-[24px] font-bold text-black">포인트상점</span>
+      </div>
+      <div className="h-[48]px flex w-full items-center justify-center">
+        <UserStatusbar point={userStatus?.point ?? 0} availablePoint={userStatus?.point ?? 0} />
+      </div>
       <BottomNavigation />
     </PageContainer>
   )
