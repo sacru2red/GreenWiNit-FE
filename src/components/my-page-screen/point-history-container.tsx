568b00f9e91be740f11bffcff4fd8cf186888bf9
junjeong <wn8624@naver.com>
2025-07-17 17:54:09 +0900

    chore: page-history-container.tsx -> mockdata인 list 타입 선언

--- src/components/my-page-screen/point-history-container.tsx
+++ src/components/my-page-screen/point-history-container.tsx
@@ -2,36 +2,44 @@ import PointsFilterButton from '@/components/my-page-screen/points-filter-button
 import PointsHistoryList from '@/components/my-page-screen/points-history-list'
 
 function PointHistoryContainer() {
+  type PointHistoryItem = {
+    pointTransactionId: string
+    description: string
+    amount: number
+    status: 'EARN' | 'SPEND'
+    transactionAt: string // ISO 문자열
+  }
+
   const list = [
     {
       pointTransactionId: '1L',
       description: '텀블러 사용 챌린지 완료',
       amount: 700,
-      status: 'EARN', // "EARN" | "SPEND"
-      transactionAt: '2025-07-12T08:43:38.908Z', // ISO 날짜 문자열
+      status: 'EARN',
+      transactionAt: '2025-07-12T08:43:38.908Z',
     },
     {
       pointTransactionId: '2l',
       description: '대중교통 이용 챌린지 완료',
       amount: 500,
-      status: 'EARN', // "EARN" | "SPEND"
-      transactionAt: '2025-06-10T08:43:38.908Z', // ISO 날짜 문자열
+      status: 'EARN',
+      transactionAt: '2025-06-10T08:43:38.908Z',
     },
     {
       pointTransactionId: '1L',
       description: '친환경 텀블러 교환',
       amount: 700,
-      status: 'SPEND', // "EARN" | "SPEND"
-      transactionAt: '2025-06-05T08:43:38.908Z', // ISO 날짜 문자열
+      status: 'SPEND',
+      transactionAt: '2025-06-05T08:43:38.908Z',
     },
     {
       pointTransactionId: '1L',
       description: '플라스틱 줄이기 챌린지 완료',
       amount: 800,
-      status: 'EARN', // "EARN" | "SPEND"
-      transactionAt: '2025-06-01T08:43:38.908Z', // ISO 날짜 문자열
+      status: 'EARN',
+      transactionAt: '2025-06-01T08:43:38.908Z',
     },
-  ]
+  ] satisfies PointHistoryItem[]
 
   return (
     <section className="flex flex-col gap-4 p-4">
