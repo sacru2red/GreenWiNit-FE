3d1ac52a808b816266452792a8018f6698964c25
sacru2red <sacru2red@gmail.com>
2025-07-20 13:14:24 +0900

    feat: 404 NotFound 페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import ManageTeam from './pages/app/challenges/[id]/teams/[id]/joined'
 import TeamModify from './pages/app/challenges/[id]/teams/[id]/modify'
 import ChallengeSubmitTeam from './pages/app/challenges/[id]/submit/team/[teamId]'
 import { Toaster } from './components/ui/sonner'
+import NotFound from './pages/404'
 
 const queryClient = new QueryClient()
 
@@ -65,7 +66,8 @@ function App() {
                 <Route path="/challenges/:challengeId/teams/:teamId" element={<TeamDetail />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/my" element={<MyPage />} />
-                <Route path="*" element={<Main />} />
+                <Route path="/" element={<Main />} />
+                <Route path="*" element={<NotFound />} />
               </Routes>
             )}
           </div>
