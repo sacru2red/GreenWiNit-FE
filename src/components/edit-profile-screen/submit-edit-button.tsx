70f8e5351f232a7bb36356c25cf0e6dce0a971d0
정준영 <wn8624@naver.com>
2025-07-23 14:52:50 +0900

    refactor: ninkname-edit-form.tsx 관련 리뷰 반영

--- src/components/edit-profile-screen/submit-edit-button.tsx
+++ src/components/edit-profile-screen/submit-edit-button.tsx
@@ -1,22 +1,9 @@
-import { MouseEvent } from 'react'
 import { Button } from '@/components/ui/button'
-import useOpenModal from '@/hooks/useOpenModal'
 
 function SubmitEditButton() {
-  const openModal = useOpenModal()
-
-  const onSubmit = () => {
-    // 회원 정보 수정 API 연결 로직 추가 예정
-  }
-
-  const showConfirmModal = (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault()
-    openModal({ group: 'confirm', type: 'edit-profile', onConfirm: onSubmit })
-  }
-
   return (
     <div className="absolute bottom-4 left-0 mt-[168px] flex w-full px-4 py-6">
-      <Button size="flex" type="submit" onClick={showConfirmModal}>
+      <Button size="flex" type="submit">
         수정하기
       </Button>
     </div>
