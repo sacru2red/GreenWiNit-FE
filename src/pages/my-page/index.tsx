d6ce28188c5d6957b7a61d4276433198fb33d8d6
정준영 <wn8624@naver.com>
2025-07-26 14:45:01 +0900

    feat: update UserCard markup and enable logout in MyPage

--- src/pages/my-page/index.tsx
+++ src/pages/my-page/index.tsx
@@ -1,30 +1,37 @@
 import UserCard from '@/components/common/UserCard'
 import { Card, CardAction, CardContent } from '@/components/ui/card'
 import MyPageLayout from '@/pages/my-page/my-page-layout'
+import { useUserStore } from '@/store/userStore'
 
 function MyPage() {
-  const cardItems = [
+  const logout = useUserStore((s) => s.logout)
+
+  const CARD_ITEMS = [
     {
-      title: 'SNS',
+      category: 'SNS',
       items: [
         { title: '인스타그램', url: '' },
         { title: '블로그', url: '' },
       ],
     },
     {
-      title: '고객센터',
+      category: '고객센터',
       items: [
         { title: '1:1문의', url: '' },
         { title: 'FAQ', url: '' },
       ],
     },
     {
-      title: '환경설정',
+      category: '환경설정',
       items: [
         { title: '약관 및 정책', url: '' },
         { title: '회원정보수정', url: '' },
         { title: '회원탈퇴', url: '' },
-        { title: '로그아웃', url: '' },
+        {
+          title: '로그아웃',
+          url: '',
+          action: logout,
+        },
       ],
     },
   ]
@@ -33,18 +40,19 @@ function MyPage() {
     <MyPageLayout title="마이페이지" navigationIsExist={true}>
       <div className="flex flex-col gap-10 px-[13px] py-[33px]">
         <UserCard />
-        {cardItems.map((cardItem, i) => {
+        {CARD_ITEMS.map((el, i) => {
           return (
             <Card key={i} className="h-fit rounded-2xl bg-white p-0 shadow-lg">
               <CardContent className="flex flex-col">
-                <div className="flex w-full flex-row items-center justify-center p-4 font-bold">
-                  {cardItem.title}
-                </div>
-                {cardItem.items.map((item, j) => {
+                <button className="flex w-full flex-row items-center justify-center p-4 font-bold">
+                  {el.category}
+                </button>
+                {el.items.map((item, j) => {
                   return (
                     <CardAction
                       key={j}
                       className="flex w-full flex-row items-center border-t-[1px] border-t-[9E9E9E] p-4"
+                      onClick={item.action}
                     >
                       <span className="text-title-smaller text-4">{item.title}</span>
                       <svg
