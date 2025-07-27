3cd9d747726a178e051eaf67398551f9ab77d29f
sacru2red <sacru2red@gmail.com>
2025-07-28 00:52:49 +0900

    feat: 시안에 맞게 디자인 수정, placeholder 값 변경, label 내용 변경

--- src/components/edit-profile-screen/nickname-form/input-nickname.tsx
+++ src/components/edit-profile-screen/nickname-form/input-nickname.tsx
@@ -1,29 +1,48 @@
-const InputNickname = () => {
+import Required from '@/components/common/Required'
+import { Fragment, useId } from 'react'
+
+interface InputNicknameProps {
+  mode?: 'new' | 'edit'
+}
+
+const InputNickname = ({ mode = 'new' }: InputNicknameProps) => {
+  const id = useId()
+
   const checkNicknameDuplication = () => {
     // 닉네임 중복 확인 api 연결 예정
   }
 
+  const labelContent =
+    mode === 'new' ? (
+      '변경할 닉네임'
+    ) : (
+      <Fragment>
+        닉네임
+        <Required />
+      </Fragment>
+    )
+
   return (
     <fieldset>
-      <legend className="sr-only">변경할 닉네임</legend>
-      <label htmlFor="new-nickname" className="text-secondary-foreground block text-start text-sm">
-        변경할 닉네임
+      <legend className="sr-only">{labelContent}</legend>
+      <label htmlFor={id} className="text-secondary-foreground block text-start text-sm">
+        {labelContent}
       </label>
-      <div className="mt-2 flex w-full overflow-hidden rounded-md border border-gray-300">
+      <div className="focus-within:border-mountain_meadow mt-2 flex w-full justify-between overflow-hidden rounded-md border border-gray-300 px-3 py-1">
         <input
-          id="new-nickname"
+          id={id}
           type="text"
-          placeholder="아이디 입력(6~20자)"
+          placeholder={mode === 'new' ? '새 닉네임을 입력해주세요.' : '닉네임 입력'}
           // 중복체크 안되었을 떄에는 keydown 막고, 체크 되었으면 keydown 허용해서 enter 치면 모달 띄우게 할 예정
           // onKeyDown={(e) => {
           //   if (e.key === 'Enter') e.preventDefault()
           // }}
-          className="flex-1 bg-white px-4 py-3 text-sm focus:outline-none"
+          className="flex-1 bg-white py-3 text-sm focus:outline-none"
         />
         <button
           type="button"
           onClick={checkNicknameDuplication}
-          className="hover:bg-mountain_meadow bg-gray-300 px-4 text-sm text-white"
+          className="hover:bg-mountain_meadow rounded-md bg-gray-300 px-8 text-sm text-white"
         >
           중복 확인
         </button>
