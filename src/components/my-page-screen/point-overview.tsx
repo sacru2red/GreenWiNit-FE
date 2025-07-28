3ce4ed55e6cb19160e654acbf36573562f4ff686
정준영 <wn8624@naver.com>
2025-07-28 14:17:18 +0900

    refactor: move default point values to rendering component

--- src/components/my-page-screen/point-overview.tsx
+++ src/components/my-page-screen/point-overview.tsx
@@ -7,20 +7,22 @@ interface PointOverviewProps {
 }
 
 function PointOverview({ points }: PointOverviewProps) {
+  const { currentBalance = 0, totalEarned = 0, totalSpent = 0 } = points ?? {}
+
   return (
     <section className="text-secondary-foreground border-lighter-gray-border flex flex-col justify-between gap-6 border-1 bg-white px-4 py-6">
       <div className="flex flex-col gap-2">
         <span className="text-sm">총 보유 포인트</span>
-        <span className="text-4xl font-bold">{points.currentBalance}</span>
+        <span className="text-4xl font-bold">{currentBalance}</span>
       </div>
       <div className="flex">
         <div className="flex flex-1 flex-col gap-1">
           <span className="text-xs">획득 포인트</span>
-          <span className="text-mountain_meadow text-lg font-bold">{points.totalEarned}</span>
+          <span className="text-mountain_meadow text-lg font-bold">{totalEarned}</span>
         </div>
         <div className="flex flex-1 flex-col gap-1">
           <span className="text-xs">사용 포인트</span>
-          <span className="text-error text-lg font-bold">{points.totalSpent}</span>
+          <span className="text-error text-lg font-bold">{totalSpent}</span>
         </div>
       </div>
     </section>
