1cc1bd4eee5166e2ce743eb9cc94e22b42920809
정준영 <wn8624@naver.com>
2025-07-28 15:22:18 +0900

    chore: remove duplicate file

--- src/types/points 2.ts
+++
@@ -1,9 +0,0 @@
-export type PointFilterType = 'all' | 'earn' | 'spend'
-
-export type PointHistory = {
-  pointTransactionId: string
-  description: string
-  amount: number
-  status: 'EARN' | 'SPEND'
-  transactionAt: string // ISO 문자열
-}
