1c3beca36c7394e9236ea6ea772610498dcab355
sacru2red <sacru2red@gmail.com>
2025-08-04 03:48:46 +0900

    feat: not fetch when accessToken is null

--- src/hooks/use-user-status.ts
+++ src/hooks/use-user-status.ts
@@ -1,12 +1,15 @@
 import { usersApi, usersQueryKeys } from '@/api/users'
 import { useQuery } from '@tanstack/react-query'
 import useUserId from './use-user-id'
+import { authStore } from '@/store/auth-store'
 
 export const useUserStatus = () => {
+  const accessToken = authStore.getState().accessToken
   const userId = useUserId()
 
   return useQuery({
     queryKey: usersQueryKeys['me/status'].detail(userId).queryKey,
     queryFn: usersApi.getUserStatus,
+    enabled: accessToken != null,
   })
 }
