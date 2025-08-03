91dfad4568bcb26b20c0d35ce9765aab51910c71
sacru2red <sacru2red@gmail.com>
2025-08-04 03:58:12 +0900

    feat: remove mocking product apis

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,6 +1,5 @@
 // https://mswjs.io/docs/integrations/browser
 import { setupWorker } from 'msw/browser'
 import { handlers } from './handlers'
-import { productHandlers } from './product-handlers'
 
-export const worker = setupWorker(...handlers, ...productHandlers)
+export const worker = setupWorker(...handlers)
