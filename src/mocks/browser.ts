426cbe529cf600ee1d79ad7a87a374c82e0c3733
sacru2red <sacru2red@gmail.com>
2025-08-04 04:29:42 +0900

    chore: uninstall msw

--- src/mocks/browser.ts
+++
@@ -1,5 +0,0 @@
-// https://mswjs.io/docs/integrations/browser
-import { setupWorker } from 'msw/browser'
-import { handlers } from './handlers'
-
-export const worker = setupWorker(...handlers)
--- src/mocks/handlers.ts
+++
@@ -1,3 +0,0 @@
-// https://mswjs.io/docs/quick-start#2-request-handlers
-
-export const handlers = []
