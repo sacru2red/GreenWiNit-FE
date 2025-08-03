0a3cf1615be1e06d8dbb2aaca43ee93a59f3c760
sacru2red <sacru2red@gmail.com>
2025-08-04 04:30:32 +0900

    fix: build fail invalid ref

--- src/main.tsx
+++ src/main.tsx
@@ -6,22 +6,8 @@ import { API_URL } from './constant/network.ts'
 import { authStore } from './store/auth-store.ts'
 import { initHistoryAndLocation } from './lib/utils.ts'
 
-async function enableMocking() {
-  if (import.meta.env.MODE === 'production') {
-    return
-  }
-
-  const { worker } = await import('./mocks/browser')
-
-  // `worker.start()` returns a Promise that resolves
-  // once the Service Worker is up and ready to intercept requests.
-  return worker.start()
-}
-
-enableMocking().then(() => {
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  createRoot(document.getElementById('root')!).render(<App />)
-})
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+createRoot(document.getElementById('root')!).render(<App />)
 
 fetchIntercept.register({
   request: function (url: string, config: RequestInit) {
