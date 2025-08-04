693ca6ffac0a1b070994d583c578be3d921bfca7
sacru2red <sacru2red@gmail.com>
2025-08-04 17:22:43 +0900

    chore: remove debugging code

--- src/main.tsx
+++ src/main.tsx
@@ -41,7 +41,6 @@ fetchIntercept.register({
         if (response.headers.get('content-type')?.includes('json')) {
           response.json().then((body) => {
             if (body.message === '접근이 거부되었습니다.' || body.message.includes('JWT 토큰')) {
-              console.log('ok...?')
               authStore.getState().initAccessToken()
               initHistoryAndLocation()
             }
