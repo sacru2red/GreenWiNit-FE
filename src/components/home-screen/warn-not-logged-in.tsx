b7ef00c510a7b94a80f294dad492bd4df0a6e620
sacru2red <sacru2red@gmail.com>
2025-08-09 01:21:44 +0900

    refactor: improve WarnNotLoggedIn component reusability

--- src/components/home-screen/warn-not-logged-in.tsx
+++ src/components/home-screen/warn-not-logged-in.tsx
@@ -1,27 +1,51 @@
 import { Dialog, DialogContent, DialogDescription, DialogFooter } from '../ui/dialog'
 import { Button } from '../ui/button'
 import { useNavigate } from 'react-router-dom'
+import { stringify } from '@/lib/query-string'
 
 interface WarnNotLoggedInProps {
   isOpen: boolean
   onOpenChange(open: boolean): void
+  message?: string
+  infoMessageAfterRedirecting?: string
+  backButtonAction?: 'close' | 'back'
 }
 
-const WarnNotLoggedIn = ({ isOpen, onOpenChange }: WarnNotLoggedInProps) => {
+const WarnNotLoggedIn = ({
+  isOpen,
+  onOpenChange,
+  message = `로그인 후,\n챌린지에 참여할 수 있어요.`,
+  infoMessageAfterRedirecting: infoMessageAfterRedirecting,
+  backButtonAction = 'close',
+}: WarnNotLoggedInProps) => {
   const navigate = useNavigate()
 
   return (
     <Dialog open={isOpen} onOpenChange={onOpenChange}>
       <DialogContent>
-        <DialogDescription className="text-center text-black">
-          로그인 후<br />
-          챌린지에 참여할 수 있어요.
+        <DialogDescription className="text-center whitespace-pre-line text-black">
+          {message}
         </DialogDescription>
         <DialogFooter className="flex flex-row gap-6 sm:justify-center">
-          <Button variant="cancel" size="flex" onClick={() => onOpenChange(false)}>
+          <Button
+            variant="cancel"
+            size="flex"
+            onClick={() => {
+              if (backButtonAction === 'close') {
+                onOpenChange(false)
+              } else {
+                navigate(-1)
+              }
+            }}
+          >
             취소
           </Button>
-          <Button size="flex" onClick={() => navigate(`/login}`)}>
+          <Button
+            size="flex"
+            onClick={() =>
+              navigate(`/login?${stringify({ message: infoMessageAfterRedirecting })}`)
+            }
+          >
             확인
           </Button>
         </DialogFooter>
