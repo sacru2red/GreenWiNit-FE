6e90f3bf17f49c2bcb6c0cafaa67f02c893a7c2a
sacru2red <sacru2red@gmail.com>
2025-08-10 22:16:02 +0900

    refactor:

--- src/components/common/form/error-message.tsx
+++ src/components/common/form/error-message.tsx
@@ -1,13 +1,13 @@
-import { Message, MultipleFieldErrors } from 'react-hook-form'
+import type { Message, MultipleFieldErrors } from 'react-hook-form'
 import { ErrorMessage as OriginalErrorMessage } from '@hookform/error-message'
-import { ComponentProps } from 'react'
+import type { ComponentProps } from 'react'
 
 const RenderMessage = (data: { message: Message; messages?: MultipleFieldErrors }) => {
   return <p className="self-start p-2 text-red-500">{data.message}</p>
 }
 
 const ErrorMessage = (props: ComponentProps<typeof OriginalErrorMessage>) => {
-  return <OriginalErrorMessage {...props} render={RenderMessage} />
+  return <OriginalErrorMessage render={RenderMessage} {...props} />
 }
 
 export default ErrorMessage
