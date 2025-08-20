67d457dedf253b342699e2f9bdf34940c06316ff
sacru2red <sacru2red@gmail.com>
2025-08-20 20:37:32 +0900

    feat: components/common/modal/dialog.tsx file shadcn overridden

---
+++ src/components/common/modal/dialog.tsx
@@ -0,0 +1,30 @@
+import {
+  Dialog as ShadcnDialog,
+  DialogClose as ShadcnDialogClose,
+  DialogContent as ShadcnDialogContent,
+  DialogDescription as ShadcnDialogDescription,
+  DialogFooter as ShadcnDialogFooter,
+  DialogHeader as ShadcnDialogHeader,
+  DialogOverlay as ShadcnDialogOverlay,
+  DialogPortal as ShadcnDialogPortal,
+  DialogTitle as ShadcnDialogTitle,
+  DialogTrigger as ShadcnDialogTrigger,
+} from '@/components/shadcn/dialog'
+import { ComponentProps } from 'react'
+
+export function DialogContent({
+  showCloseButton = false,
+  ...restProps
+}: ComponentProps<typeof ShadcnDialogContent>) {
+  return <ShadcnDialogContent {...restProps} showCloseButton={showCloseButton} />
+}
+
+export const Dialog = ShadcnDialog
+export const DialogClose = ShadcnDialogClose
+export const DialogDescription = ShadcnDialogDescription
+export const DialogFooter = ShadcnDialogFooter
+export const DialogHeader = ShadcnDialogHeader
+export const DialogOverlay = ShadcnDialogOverlay
+export const DialogPortal = ShadcnDialogPortal
+export const DialogTitle = ShadcnDialogTitle
+export const DialogTrigger = ShadcnDialogTrigger
